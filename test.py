23832a3b3663f3ffc4264e6373b14d20f0ccfc0c
cmathi1386 <156830461+cmathi1386@users.noreply.github.com>
2024-08-21 22:38:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from requests.packages.urllib3.util.retry import Retry
 
 # List of target URLs
 urls = [
-    'http://skillrack.com',
-    'http://skillrack.com'
+    'http://rmk685.examly.io',
+    'http://rmk685.examly.io'
 ]
 
 # The number of requests to send per second for each URL
